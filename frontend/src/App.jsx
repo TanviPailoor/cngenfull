2d89655eccd2b5b92c66f33b5e148c6b8a50e3b0
TanviPailoor <tanvipailoor@gmail.com>
2024-09-02 17:33:46 +0530

    added all folders to the repo

---
+++ frontend/src/App.jsx
@@ -0,0 +1,145 @@
+import io from "socket.io-client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {
+	Parent,
+	Register,
+	Login,
+	EditProfile,
+	Schedules,
+	Referral,
+	Notification,
+	Home,
+	Profile,
+	Tasks,
+	Employees,
+	MainLogin,
+	Admin,
+} from "./employee/pages/index";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import ProfileDrawer from "./user/userComponents/profileDrawer/ProfileDrawer";
+import VehicleDetails from "./user/userComponents/vehicledetails/VehicleDetails";
+import Book from "./user/userComponents/books/Book";
+import PrivateRoute from "./user/userComponents/PrivateRoute";
+import NotificationPage from "./user/userPages/notificationpage/NotificationPage";
+import BookingPage from "./user/userPages/Bookingpage/BookingPage";
+import EditLocationPage from "./user/userPages/editlocationpage/EditLocation";
+import FuelPrice from "./user/userPages/fuelprice/FuelPrice";
+import UHome from "./user/userPages/home/UHome";
+import ULogin from "./user/userPages/login/ULogin";
+import ProfilePage from "./user/userPages/profilepage/ProfilePage";
+import ServicesPage from "./user/userPages/servicespage/ServicesPage";
+import SettingPage from "./user/userPages/settingspage/SettingPage";
+import URegister from "./user/userComponents/register/URegister";
+const App = () => {
+	const { loginInfo } = useSelector((state) => state.aslice);
+	const socket = io("http://localhost:8600");
+	useEffect(() => {
+		console.log(loginInfo);
+		if (loginInfo) {
+			socket.emit("joinRoom", loginInfo?._id);
+			socket.on("connect", () => {
+				console.log("Connected to the server");
+			});
+		}
+		return () => {
+			socket.disconnect();
+		};
+	}, [socket]);
+	return (
+		<section className="font">
+			<BrowserRouter>
+				<Routes>
+					{/* <Route path="/" element={<SplashScreen />} /> */}
+					<Route path="/mainLogin" element={<MainLogin />} />
+					<Route path="/emp-register" element={<Register />} />
+					<Route path="/emp-login" element={<Login />} />
+					<Route path="/" element={<Parent />}>
+						<Route index element={<Home socket={socket} />} />
+						<Route path="profile" element={<Profile />} />
+						<Route path="profile/editprofile" element={<EditProfile />} />
+						<Route path="schedule" element={<Schedules />} />
+						<Route path="performance" element={<Performance />} />
+						<Route path="referral" element={<Referral />} />
+						<Route path="notification" element={<Notification />} />
+						<Route path="admin/*" element={<Admin socket={socket} />}>
+							<Route path="employees" element={<Employees />} />
+							<Route path="tasks" element={<Tasks />} />
+						</Route>
+					</Route>
+					<Route path="/user-login" element={<ULogin />} />
+					<Route path="/user-register" element={<URegister />} />
+					<Route
+						path="/user-home"
+						element={
+							<PrivateRoute>
+								<UHome />
+							</PrivateRoute>
+						}
+					/>
+					<Route path="/notifications" element={<NotificationPage />} />
+					<Route path="/booking" element={<BookingPage />} />
+					<Route path="/services" element={<ServicesPage />} />
+					<Route
+						path="/myprofile"
+						element={
+							<PrivateRoute>
+								<ProfilePage />
+							</PrivateRoute>
+						}
+					/>
+					<Route
+						path="/location"
+						element={
+							<PrivateRoute>
+								<EditLocationPage />
+							</PrivateRoute>
+						}
+					/>
+					<Route
+						path="/settings"
+						element={
+							<PrivateRoute>
+								<SettingPage />
+							</PrivateRoute>
+						}
+					/>
+					<Route
+						path="/profile"
+						element={
+							<PrivateRoute>
+								<ProfileDrawer />
+							</PrivateRoute>
+						}
+					/>
+					<Route
+						path="/fuelprice"
+						element={
+							<PrivateRoute>
+								<FuelPrice />
+							</PrivateRoute>
+						}
+					/>
+					<Route
+						path="/vehicle/:type"
+						element={
+							<PrivateRoute>
+								<VehicleDetails />
+							</PrivateRoute>
+						}
+					/>
+					<Route
+						path="/book"
+						element={
+							<PrivateRoute>
+								<Book />
+							</PrivateRoute>
+						}
+					/>
+				</Routes>
+			</BrowserRouter>
+		</section>
+	);
+};
+
+export default App;
---
+++ frontend/src/employee/components/navbar/Navbar.jsx
@@ -0,0 +1,111 @@
+// import React from 'react';
+// import { AiFillHome } from 'react-icons/ai';
+// import { FaCalendarAlt } from 'react-icons/fa';
+// import { MdBarChart } from 'react-icons/md';
+// import { IoPerson } from 'react-icons/io5';
+// import { Link, useLocation } from 'react-router-dom';
+// import './Navbar.scss';
+
+// const Navbar = () => {
+//   const location = useLocation();
+
+//   const userId = localStorage.getItem('userId');
+
+//   return (
+//     <section className="nav-section">
+//       <Link
+//         to="/"
+//         className={`nav-link ${
+//           location.pathname === '/home' ? 'active-link' : ''
+//         }`}
+//       >
+//         <AiFillHome />
+//       </Link>
+//       <Link
+//         to="schedule"
+//         className={`nav-link ${
+//           location.pathname === '/schedule' ? 'active-link' : ''
+//         }`}
+//       >
+//         <FaCalendarAlt />
+//       </Link>
+//       <Link
+//         to="performance"
+//         className={`nav-link ${
+//           location.pathname === '/performance' ? 'active-link' : ''
+//         }`}
+//       >
+//         <MdBarChart />
+//       </Link>
+//       <Link
+//         to={`private/profile`}
+//         className={`nav-link ${
+//           location.pathname === `/profile/${userId ? userId : ''}`
+//             ? 'active-link'
+//             : ''
+//         }`}
+//       >
+//         <IoPerson />
+//       </Link>
+//     </section>
+//   );
+// };
+
+// export default Navbar;
+
+import React from 'react';
+import { AiFillHome } from 'react-icons/ai';
+import { FaCalendarAlt } from 'react-icons/fa';
+import { MdBarChart } from 'react-icons/md';
+import { IoPerson } from 'react-icons/io5';
+import { IoIosArrowBack } from "react-icons/io";
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import './Navbar.scss';
+
+const Navbar = () => {
+  const location = useLocation();
+  const userId = localStorage.getItem('userId');
+  const navigate = useNavigate();
+  return (
+		<section className="nav-section">
+			<Link
+				className="nav-link"
+				onClick={() => navigate(-1)}
+			>
+				<IoIosArrowBack />
+			</Link>
+			<Link
+				to="/"
+				className={`nav-link ${location.pathname === "/" ? "active-link" : ""}`}
+			>
+				<AiFillHome />
+			</Link>
+			<Link
+				to="/schedule"
+				className={`nav-link ${
+					location.pathname === "/schedule" ? "active-link" : ""
+				}`}
+			>
+				<FaCalendarAlt />
+			</Link>
+			<Link
+				to="/performance"
+				className={`nav-link ${
+					location.pathname === "/performance" ? "active-link" : ""
+				}`}
+			>
+				<MdBarChart />
+			</Link>
+			<Link
+				to="/profile"
+				className={`nav-link ${
+					location.pathname === `/profile` ? "active-link" : ""
+				}`}
+			>
+				<IoPerson />
+			</Link>
+		</section>
+	);
+};
+
+export default Navbar;
